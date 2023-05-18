b22fdfc705c4d2774c8f52548a56eb5ec72c5922
Your Name <you@example.com>
2023-05-18 09:53:25 +0700

    change

--- src/router/index.js
+++ src/router/index.js
@@ -6,75 +6,75 @@ Vue.use(VueRouter)
 Vue.use(Meta)
 
 const routes = [
+  // {
+  //   path: '/',
+  //   name: 'Home',
+  //   component: () => import('../views/Home.vue'),
+  //   meta: {
+  //     header: true
+  //   }
+  // },
   {
-    path: '/',
-    name: 'Home',
-    component: () => import('../views/Home.vue'),
-    meta: {
-      header: true
-    }
-  },
-  {
-    path: '/kyc/:id',
+    path: '/:id',
     name: 'Kyc',
     component: () => import('../views/Kyc.vue')
   },
-  {
-    path: '/danh-muc/:id',
-    component: () => import('../views/Blog.vue'),
-    meta: {
-      header: true
-    }
-  },
-  {
-    path: '/tim-kiem/:id',
-    component: () => import('../views/Blog.vue'),
-    meta: {
-      header: true
-    }
-  },
-  {
-    path: '/tin-tuc',
-    component: () => import('../views/Blog.vue'),
-    meta: {
-      header: true
-    }
-  },
-  {
-    path: '/ngoai-te',
-    component: () => import('../views/rate/Currency.vue'),
-    meta: {
-      header: true
-    }
-  },
-  {
-    path: '/gia-vang',
-    component: () => import('../views/rate/Gold.vue'),
-    meta: {
-      header: true
-    }
-  },
-  {
-    path: '/tien-dien-tu',
-    component: () => import('../views/rate/Crypto.vue'),
-    meta: {
-      header: true
-    }
-  },
-  {
-    path: '/chung-khoan',
-    component: () => import('../views/rate/Stock.vue'),
-    meta: {
-      header: true
-    }
-  },
-  {
-    path: '/:id',
-    component: () => import('../views/Detail.vue'),
-    meta: {
-      header: true
-    }
-  },
+  // {
+  //   path: '/danh-muc/:id',
+  //   component: () => import('../views/Blog.vue'),
+  //   meta: {
+  //     header: true
+  //   }
+  // },
+  // {
+  //   path: '/tim-kiem/:id',
+  //   component: () => import('../views/Blog.vue'),
+  //   meta: {
+  //     header: true
+  //   }
+  // },
+  // {
+  //   path: '/tin-tuc',
+  //   component: () => import('../views/Blog.vue'),
+  //   meta: {
+  //     header: true
+  //   }
+  // },
+  // {
+  //   path: '/ngoai-te',
+  //   component: () => import('../views/rate/Currency.vue'),
+  //   meta: {
+  //     header: true
+  //   }
+  // },
+  // {
+  //   path: '/gia-vang',
+  //   component: () => import('../views/rate/Gold.vue'),
+  //   meta: {
+  //     header: true
+  //   }
+  // },
+  // {
+  //   path: '/tien-dien-tu',
+  //   component: () => import('../views/rate/Crypto.vue'),
+  //   meta: {
+  //     header: true
+  //   }
+  // },
+  // {
+  //   path: '/chung-khoan',
+  //   component: () => import('../views/rate/Stock.vue'),
+  //   meta: {
+  //     header: true
+  //   }
+  // },
+  // {
+  //   path: '/:id',
+  //   component: () => import('../views/Detail.vue'),
+  //   meta: {
+  //     header: true
+  //   }
+  // },
 ]
 
 const router = new VueRouter({
